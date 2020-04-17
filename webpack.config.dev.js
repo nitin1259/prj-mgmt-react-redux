7e2f7c248f8e782c3ee216136c07b18b17e626c4
Nitin SINGH <nitinkumar.singh@f5.com>
2020-04-18 01:44:52 +0530

    start webpack via npm script

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,7 +7,7 @@ process.env.NODE_ENV = "development";
 module.exports = {
   mode: "development",
   target: "web",
-  devTool: "cheap-module-source-map",
+  devtool: "cheap-module-source-map",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"),
@@ -16,18 +16,18 @@ module.exports = {
   },
   devServer: {
     stats: "minimal",
-    overlay: "true",
-    historyApiFallback: "true",
-    dispalyHostCheck: "true",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
     headers: {
       "Access-Control-Allow-Origin": "*",
     },
-    https: "false",
+    https: false,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      favicon: ".src/favicon.ico",
+      favicon: "./src/favicon.ico",
     }),
   ],
   module: {
