750f1f68b02abfffa2d5cab897744b0214d51ae7
Nitin SINGH <nitinkumar.singh@f5.com>
2020-04-18 01:06:36 +0530

    webpack core config settings

---
+++ webpack.config.dev.js
@@ -0,0 +1,17 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devTool: "cheap-module-source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+};
