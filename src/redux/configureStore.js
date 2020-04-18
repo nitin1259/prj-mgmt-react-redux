3b3da82331ba6f70d0f452583e7f443bc73a5517
Nitin SINGH <nitinkumar.singh@f5.com>
2020-04-18 22:55:27 +0530

    add middleware thunk to configure store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducer/reducers";
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant"; //it will notify(warn) us if we accidentally mutate redux state..
+import thunk from "redux-thunk"; // redux middleware is a way to enhance the redux behaviour
 
 export default function configureStore(initialState) {
   const enhanceCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support to Redux Dev tool
@@ -8,6 +9,6 @@ export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    enhanceCompose(applyMiddleware(reduxImmutableStateInvariant()))
+    enhanceCompose(applyMiddleware(thunk, reduxImmutableStateInvariant()))
   );
 }
