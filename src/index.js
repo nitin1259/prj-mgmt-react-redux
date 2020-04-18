985c7fe800b97d47a2740aaea5865b1b1822611c
Nitin SINGH <nitinkumar.singh@f5.com>
2020-04-18 16:33:45 +0530

    instantiate store and provider

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./component/App";
 import { BrowserRouter as Router } from "react-router-dom";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
+
+const store = configureStore();
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("app")
 );
