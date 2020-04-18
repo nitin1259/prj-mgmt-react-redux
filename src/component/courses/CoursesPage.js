8ed422c19e620388094feec24f8dc55a6dadbb2a
Nitin SINGH <nitinkumar.singh@f5.com>
2020-04-18 15:49:32 +0530

    introducing state to the Course Page

---
+++ src/component/courses/CoursesPage.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class CoursesPage extends Component {
+  state = {
+    course: {
+      title: "",
+    },
+  };
+
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+  render() {
+    return (
+      <form>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
+  }
+}
+
+export default CoursesPage;
